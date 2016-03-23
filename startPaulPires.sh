bb0f5a9dc484a9ce261e895e502b864d3abe777c
Paul Pires <paul@paulpires.com>
2016-03-23 14:36:57 +0000

    modified start script for prod .... need to move ghost outside of mode_modules for custom shit

--- startPaulPires.sh
+++ startPaulPires.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export PORT=80
-export NODE_ENV='prod'
-forever start ./bin/www
+export NODE_ENV='production'
+rm ~/foreverOut
+rm ~/stdOut
+rm ~/stdErr
+forever -l ~/foreverOut -o ~/stdOut -e ~/stdErr start ./bin/www
 # npm start
