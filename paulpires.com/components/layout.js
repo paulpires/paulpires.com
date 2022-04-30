384986dd0746e18e8f114c381d56de0d438447a8
paulpires <paulpires101@gmail.com>
2022-04-30 13:07:47 +0100

    add a responsive design

--- paulpires.com/components/layout.js
+++ paulpires.com/components/layout.js
@@ -2,22 +2,35 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Layout({ children }) {
+const containerStyle = `
+  container
+  md:shadow-xl
+  md:border-1
+  md:mx-auto
+  sm:max-w-3xl
+  mx-auto
+  bg-white
+  px-4
+  pt-6
+  md:mt-8
+`
+export default function Layout({ children })
+{
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='container shadow-xl border-1 bg-white mx-auto sm:max-w-none md:max-w-3xl lg:max-w-3x px-4 pt-6 mt-5'>
-        <header className='flex justify-between pb-2'>
-          <h1 className="text-3xl font-black">
+      <div className={containerStyle}>
+        <header className='sm:flex sm:flex-row sm:justify-between flex flex-col items-center pb-2'>
+          <h1 className="text-3xl font-black sm:pb-0 pb-2">
             Paul Pires
           </h1>
           <ul className='flex flex-row gap-3'>
             <ImageLink image="/linkedin.svg" href={'https://www.linkedin.com/in/paulpires/'} />
             <ImageLink image="/github.svg" href='https://github.com/paulpires' />
             <ImageLink image="/twitter.svg" href='https://twitter.com/paul_pires' />
-            <Link href={'/resume.pdf'} passHref><ImageLink image="/cv.svg"/></Link>
+            <Link href={'/resume.pdf'} passHref><ImageLink image="/cv.svg" /></Link>
           </ul>
         </header>
         <hr></hr>
@@ -30,15 +43,16 @@ export default function Layout({ children }) {
   )
 }
 
-function ImageLink(props) {
+function ImageLink(props) 
+{
   return (
-      <a href={props.href} target='_blank'>
-        <Image
-          priority
-          src={props.image}
-          height={40}
-          width={40}
-        />
-      </a>
+    <a href={props.href} target='_blank'>
+      <Image
+        priority
+        src={props.image}
+        height={40}
+        width={40}
+      />
+    </a>
   )
 }
--- paulpires.com/pages/index.js
+++ paulpires.com/pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Layout from '../components/layout'
 
 export default function Home() {
   return (
     <Layout>
-      <section className='py-4'>
+      <section className='py-4 font-mono text-sm'>
         <p>
           Hey, I'm Paul - a software engineer working remote in London 🇬🇧 with
           a particular interest in mobile and web technologies. I currently
@@ -13,10 +11,19 @@ export default function Home() {
         </p>
         <br></br>
         <p>
-          Feel free to contact me at paulpires101@gmail.com or connect with me 
+          Feel free to contact me at <ExtraBold>paulpires101@gmail.com</ExtraBold> or connect with me 
           on LinkedIn
         </p>
       </section>
     </Layout>
+  )
+}
+
+function ExtraBold({children}) 
+{
+  return (
+    <b className='font-extrabold'>
+      {children}
+    </b>
   )
 }
