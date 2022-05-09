b6c3850554768366edbe8d764ecf7ba4cc6a316a
paulpires <paulpires101@gmail.com>
2022-05-09 18:44:56 +0100

    remove usage of <a> on the back home button

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -41,8 +41,8 @@ export default function Post({ postData }) {
       </article>
       <div className='mt-6'><Divider/></div>
       <div className='pt-4 text-center dark:text-white'>
-        <Link href='/' passHref>
-          <BoldLink href='/'>Back home</BoldLink>
+        <Link href='/'>
+          <span className='hover:underline underline-offset-4 decoration-dashed font-extrabold'>Back home</span>
         </Link>
       </div>
     </Layout>
