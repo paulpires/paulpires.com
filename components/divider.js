24eda25db25afc4b4b5d19ed6bf1bc815edb7c52
paulpires <paulpires101@gmail.com>
2022-05-09 15:26:49 +0100

    use className

--- components/divider.js
+++ components/divider.js
@@ -1,7 +1,7 @@
 export default function Divider(props) {
     return (
-        <div class="flex">
-            <div class="flex-grow border-t border-[#ededed] dark:border-white/60"></div>
+        <div className="flex">
+            <div className="flex-grow border-t border-[#ededed] dark:border-white/60"></div>
         </div>
     )
 }
